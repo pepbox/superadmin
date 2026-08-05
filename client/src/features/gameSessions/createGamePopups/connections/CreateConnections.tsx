a86895da86c42d99e8019032ee46b6ca7ecefef7
AmitSunda1 <amitsunda14374@gmail.com>
2026-08-05 14:57:10 +0530

    the human connect changed to Konnect

--- client/src/features/gameSessions/createGamePopups/connections/CreateConnections.tsx
+++ client/src/features/gameSessions/createGamePopups/connections/CreateConnections.tsx
@@ -58,7 +58,7 @@ const CreateConnections: React.FC<GameCreationComponentProps> = ({
     <div className="w-full max-w-lg mx-auto bg-white rounded-xl p-4 sm:p-8 px-3">
       <div className="flex justify-between items-center mb-6">
         <div className="text-center w-full">
-          <h2 className="text-2xl sm:text-3xl font-bold">The Human Konnect</h2>
+          <h2 className="text-2xl sm:text-3xl font-bold">Konnect</h2>
           <h3 className="text-xl sm:text-2xl font-semibold">
             Create New Session
           </h3>
--- client/src/features/gameSessions/gamesConfig.ts
+++ client/src/features/gameSessions/gamesConfig.ts
@@ -24,7 +24,7 @@ export const games = [
     creationPopup: CreateGetSetKnow,
   },
   {
-    name: "The Human Konnect",
+    name: "Konnect",
     id: "connections",
     creationPopup: CreateConnections,
   },
